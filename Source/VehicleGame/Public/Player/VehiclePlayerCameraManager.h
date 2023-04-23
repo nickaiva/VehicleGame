b36b61df80e84ce1cd67f97eaeac84beb7db0d10
Nick Aiva <nickaiva@yahoo.gr>
2023-04-23 13:30:19 +0300

    Initial release

---
+++ Source/VehicleGame/Public/Player/VehiclePlayerCameraManager.h
@@ -0,0 +1,12 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "VehiclePlayerCameraManager.generated.h"
+
+UCLASS()
+class AVehiclePlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_UCLASS_BODY()
+
+};
